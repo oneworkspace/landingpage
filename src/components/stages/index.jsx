fddb38f5ec2e2fc37b97ff4053d54f2ae785c90d
Aditya Verma <siradityaverma@gmail.com>
2020-11-04 02:05:47 +0530

    Add connector line, change color

--- src/components/stages/index.jsx
+++ src/components/stages/index.jsx
@@ -1,23 +1,33 @@
 import React, { useState } from "react"
+import "./styles.css"
 import Auth from "../../images/auth.png"
 
 export default function Stages() {
   const [state, setState] = useState(0)
   return (
     <section className="flex container mx-auto items-center my-32">
-      <section className="flex flex-col w-2/12 mr-16">
+      <section className="flex flex-col w-2/12 mr-16 relative">
+        <div className="connector-line" />
         {["one", "two", "three", "four", "five"].map((item, index) => (
-          <span
-            className={`capitalize cursor-pointer text-center text-xl rounded-full my-4 px-4 py-2 ${
-              index === state
-                ? "bg-purple-600 text-white border border-purple-600"
-                : "border-black border"
-            }`}
-            key={index}
-            onClick={() => setState(index)}
-          >
-            Stage {item}
-          </span>
+          <div key={index} className="flex items-center">
+            <div
+              className={`h-4 w-4 mr-4 rounded-full ${
+                index === state
+                  ? "stage-selected text-white"
+                  : "stage-not-highlighted"
+              }`}
+            />
+            <span
+              className={`capitalize cursor-pointer text-center text-xl rounded-full w-3/4 my-4 px-4 py-2 ${
+                index === state
+                  ? "stage-selected text-white"
+                  : "stage-not-highlighted"
+              }`}
+              onClick={() => setState(index)}
+            >
+              Stage {item}
+            </span>
+          </div>
         ))}
       </section>
       <img src={Auth} alt="Stages" className="w-10/12" />
